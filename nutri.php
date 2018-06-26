e532dcb20b0eeb5a6d53ab007a9f82fce6ecb5a3
Ryan-Willpower <surasak.c.work@gmail.com>
2018-06-26 13:46:13 +0700

    Prepare to delete.

--- nutri.php
+++ nutri.php
@@ -3,7 +3,7 @@
 	Template Name: nutri
 	Template Post Type: page
 */
-    get_header(); 
+    get_header();
     $args1 = array( 'post_type' => 'attachment', 'post_status' => null, 'post_parent' => $post->ID, 'order' => 'ASC' );
     $attachments = get_posts( $args1 );
     global $post;
@@ -16,18 +16,18 @@
         </div>
         <div class="row">
             <div class="col-lg-3 my-3">
-                <?php 
+                <?php
                 foreach ( $attachments as $attachment ) :
                 echo wp_get_attachment_image( $attachment->ID, array(200, 200), false, array('class' => 'img-thumbnail') );
                 endforeach;?>
             </div>
             <div class="col-lg-9 h-100">
-                <?php 
+                <?php
                     if (have_posts()) {
                         while (have_posts()) {
                             the_post();
                             $content = do_shortcode(get_the_content());
-                            $content = preg_replace("/<img[^>]+\>/i", " ", $content);         
+                            $content = preg_replace("/<img[^>]+\>/i", " ", $content);
                             $content = apply_filters('the_content', $content);
                             $content = str_replace(']]>', ']]>', $content);
                             echo $content;
