b95577a951b1aa7602c2422c26ff372b53ec5439
Ryan-Willpower <surasak.c.work@gmail.com>
2018-06-26 13:45:57 +0700

    Fixed bug.

--- single.php
+++ single.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 <div class="container my-5 post-content">
-    <?php 
+    <?php
         the_title('<h5 class="title">', '</h5>');
         while ( have_posts() ) : the_post();
             the_content();
