4452a0d834efec5a0f144f7bfc855020e9955f8e
Ryan-Willpower <surasak.c.work@gmail.com>
2018-06-26 17:23:04 +0700

    Fix some security issue.

--- functions.php
+++ functions.php
@@ -190,4 +190,15 @@ function title() {
         echo (bloginfo('name'));
     }
 }
+
+// Fixed redirect
+function redirect_to_home_if_author_parameter() {
+  	$is_author_set = get_query_var( 'author', '' );
+  	if ( $is_author_set != '' && !is_admin()) {
+  		wp_redirect( home_url(), 301 );
+  		exit;
+  	}
+}
+add_action( 'template_redirect', 'redirect_to_home_if_author_parameter' );
+
 ?>
